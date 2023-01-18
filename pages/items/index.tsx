86040c902006035bfa609d998690a6b015a2d633
Shuhei Hayakawa <shuhei.hayakawa@rakus-partners.co.jp>
2023-01-18 16:33:14 +0900

    ReduxのstateをSession Storageとして保持できるようにした

--- pages/items/index.tsx
+++ pages/items/index.tsx
@@ -20,7 +20,8 @@ import { supabase } from "../../utils/supabase";
 import React from 'react';
 import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
-import { store } from '../../redux/store'
+import { store, persistor } from '../../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux"
 import ItemIndex from "../../components/ItemIndex";
 
@@ -48,10 +49,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 
-const index: NextPage = ({data3}: any) => {
+const index: NextPage = ({ data3 }: any) => {
   return (
     <Provider store={store}>
-      <ItemIndex data3={data3} />
+      <PersistGate loading={null} persistor={persistor}>
+        <ItemIndex data3={data3} />
+      </PersistGate>
     </Provider>
   )
 }
